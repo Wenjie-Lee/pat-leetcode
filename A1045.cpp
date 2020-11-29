69be76cc895df8b2e1d0b69e7e0688c1a3c19d0e
Lee-kraken <youzhaxiaoniqiu@gmail.com>
2020-11-29 23:03:30 +0800

    A1045 II
    2020/11/29

--- A1045.cpp
+++ A1045.cpp
@@ -4,15 +4,13 @@
 按照一个给定的序列O来确定，且要按照O的顺序
 **有没有可能A中没有O的某一个数字
 
-解题思路1：最长不下降子序列LIS
-
 *1
 6
 5 2 3 1 5 6
 12 2 2 4 1 5 5 6 3 1 1 5 6
 :7
-*2
 
+解题思路1：最长不下降子序列LIS
 */
 const int maxn = 210, maxl = 10010;     // stripe, dp的最大长度是maxl
 int n, m, l, stripe[maxl], dp[maxl], order[maxn];
@@ -47,5 +45,43 @@ int main() {
         ans = max(ans, dp[i]);
     }
     printf("%d", ans);
+    return 0;
+}
+
+/*
+解题思路2：最长公共子序列LCS
+        常规LCS是基于两个序列的一一不重复对应，A[i]==B[j],则dp[i][j]==dp[i-1][j-1]+1；
+        但此题可以重复对应，dp[2][2]可以被dp[2][1]和dp[1][2]同时影响，而不再仅仅是dp[1][1]；
+        即A[i]==B[j],则dp[i][j]==max(dp[i][j-1], dp[i-1][j])+1。
+*/
+const int maxn = 210, maxl = 10010;
+int n, m, l, dp[maxl][maxl], stripe[maxl], order[maxn];
+
+int main() {
+    scanf("%d%d", &n, &m);
+    for (int i = 1; i <= m; i++)
+        scanf("%d", &order[i]);
+    scanf("%d", &l);
+    for (int i = 1; i <= l; i++)
+        scanf("%d", &stripe[i]);
+
+    // 初始化边界 dp[i][0]=dp[0][j] = 0, i<=m,j<=t 
+    for (int i = 0; i <= m; i++)
+        dp[i][0] = 0;
+    for (int i = 0; i <= l; i++)
+        dp[0][i] = 0;
+    int tmp;
+    for (int i = 1; i <= m; i++)
+    {
+        for (int j = 1; j <= l; j++)
+        {
+            tmp = max(dp[i][j - 1], dp[i - 1][j]);
+            if (order[i] == stripe[j])
+                dp[i][j] = tmp + 1;
+            else
+                dp[i][j] = tmp;
+        }
+    }
+    printf("%d", dp[m][l]);
     return 0;
 }
